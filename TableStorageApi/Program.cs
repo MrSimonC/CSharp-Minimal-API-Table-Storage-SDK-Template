d832cff7cab025af6ff2658d38d510df054f1e03
Simon Crouch <simon.crouch@malvernpanalytical.com>
2022-12-12 13:48:00 +0000

    Add project files.

---
+++ TableStorageApi/Program.cs
@@ -0,0 +1,40 @@
+using Azure.Data.Tables;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+var tableConnString = builder.Configuration["TableStorageConnectionString"] ?? throw new NullReferenceException("need conn string");
+var tableName = "ProgramStatus";
+var partitionKey = "ProgramStatus";
+var tableClient = new TableClient(tableConnString, tableName);
+
+app.MapGet("/", () => Results.Json<List<StatusRecord>>(GetStatuses(tableClient, tableName)));
+app.MapPost("/", (StatusRecord record) => AddEntityToTable(tableClient, record, partitionKey));
+
+app.Run();
+
+static void AddEntityToTable(TableClient tableClient, StatusRecord record, string partitionKey)
+{
+    var entity = new TableEntity(partitionKey, Guid.NewGuid().ToString())
+    {
+        { "Program", record.Program },
+        { "Date", record.Date },
+        { "Message", record.Message }
+    };
+    tableClient.AddEntity(entity);
+}
+
+static List<StatusRecord> GetStatuses(TableClient tableClient, string partitionKey)
+{
+    var queryResultsFilter = tableClient.Query<TableEntity>(filter: $"PartitionKey eq '{partitionKey}'");
+    var result = new List<StatusRecord>();
+    foreach (var item in queryResultsFilter)
+    {
+        result.Add(new StatusRecord(item.GetString("Program"), item.GetDateTime("Date") ?? DateTime.MinValue, item.GetString("Message")));
+    }
+
+    return result;
+}
+
+public record StatusRecord(string Program, DateTime Date, string Message);
