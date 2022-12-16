6b6f5fed5f7b4d87328f4ed9588512524c6c7202
Mr Simon C <iamontheinternet@yahoo.com>
2022-12-16 11:27:02 +0000

    add api_key middleware

--- TableStorageApi/Program.cs
+++ TableStorageApi/Program.cs
@@ -1,10 +1,13 @@
 using Azure.Data.Tables;
+using Middleware; // needed for app.UseApiKeyAuthentication()
 using TableStorageApi.Models;
 
 const string tableName = "ProgramStatus";
 const string partitionKey = "StatusUpdate"; 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+// app.UseApiKeyAuthentication(); // optionally use X-API-KEY authentication referencing value in Environment Variable API_KEY
+app.Urls.Add("http://0.0.0.0:8080");
 
 var tableConnString = builder.Configuration["TableStorageConnectionString"];
 var tableClient = new TableClient(tableConnString, tableName);
